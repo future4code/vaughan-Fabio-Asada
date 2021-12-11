57342fc5d7be51282c02ac4433f5d3429cbcc96b
Fábio Asada <fabioasada@hotmail.com>
2021-12-11 11:11:07 -0300

    Versão Final!

--- modulo1/projeto-blackjack/desafio.js
+++ modulo1/projeto-blackjack/desafio.js
@@ -1,30 +1,23 @@
 
 // 2° Versão
-
 const mostrarMensagem = () => {
    const cartaUsuario = comprarCarta(); 
    const cartaUsuario2 = comprarCarta(); 
    const pontuacaoDoUsuario= cartaUsuario.valor + cartaUsuario2.valor;
 
-   console.log(`Usuário - cartas: ${cartaUsuario.texto} ${cartaUsuario2.texto} - Pontuação ${pontuacaoDoUsuario}`);
-
    const cartaComputador = comprarCarta(); 
    const cartaComputador2 = comprarCarta(); 
    const pontuacaoDoComputador= cartaComputador.valor + cartaComputador2.valor;
    
-   console.log(`Computador - cartas: ${cartaComputador.texto} ${cartaComputador2.texto} - Pontuação ${pontuacaoDoComputador}`);
-
    return [pontuacaoDoUsuario, cartaUsuario, cartaUsuario2, pontuacaoDoComputador, cartaComputador, cartaComputador2];
-}
-
-
+};
 
 const blackJack2= () => {
-   console.log("Boas vindas ao jogo de Blackjack!");
    const soma= [];
+   const somaComputador= [];
 
    if(confirm(`Boas vindas ao jogo de Blackjack!\nQuer iniciar uma nova rodada?`)) {
-    
+
       const [
          pontuacaoDoUsuario, 
          cartaUsuario, 
@@ -34,7 +27,8 @@ const blackJack2= () => {
          cartaComputador2
       ] = mostrarMensagem();
 
-      // Implementação
+      soma.push(cartaUsuario.texto, cartaUsuario2.texto);
+      somaComputador.push(cartaComputador.texto, cartaComputador2.texto);
       
       // Se as 2 cartas forem iguais a AA;
       if(pontuacaoDoUsuario === 22 || pontuacaoDoComputador === 22){
@@ -54,29 +48,67 @@ const blackJack2= () => {
          };
       };
 
-      if(confirm(`Suas cartas são ${cartaUsuario.texto} ${cartaUsuario2.texto}. A carta revelada do computador é ${cartaComputador.texto}.\nDeseja comprar mais uma carta?`)){
+      let pontos= pontuacaoDoUsuario;
+      let pontosComputador= pontuacaoDoComputador;
+
+      while (pontos < 21) {
          
+         const [
+            pontuacaoDoUsuario, 
+            cartaUsuario, 
+            cartaUsuario2, 
+            pontuacaoDoComputador, 
+            cartaComputador, 
+            cartaComputador2
+         ] = mostrarMensagem();
+         
+         pontos += cartaUsuario.valor;
+         
+         if(confirm(`Suas cartas são ${soma}. A carta revelada do computador é ${cartaComputador.texto}.\nDeseja comprar mais uma carta?`)){
+            soma.push(cartaUsuario.texto);
+            
+            if(pontos > 21){
+               
+               alert(`Usuário - cartas: ${soma} - Pontuação ${pontos}\nComputador - cartas: ${cartaComputador.texto} ${cartaComputador2.texto} - Pontuação ${pontuacaoDoComputador}\nComputador Ganhou!`);
+
+            }
+         
+         }else{
+
+            while(pontosComputador <= 21){
+               pontosComputador += cartaComputador.valor;
+
+               if(confirm(`Usuário - cartas: ${soma} - Pontuação ${pontos - cartaUsuario.valor}\nCartas do computador são ${somaComputador}.\nDeseja comprar mais uma carta?`)){
+                  somaComputador.push(cartaComputador.texto);
+
+                  if(pontosComputador >= 21){
+                     
+                     alert(`Usuário - cartas: ${soma} - Pontuação ${pontos - cartaUsuario.valor}\nComputador - cartas: ${somaComputador} - Pontuação ${pontosComputador}\nUsuário Ganhou!`);
    
+                  }
+               }else{
+                  
 
+                  if(pontos - cartaUsuario.valor > pontosComputador - cartaComputador.valor){
 
-      }else{
-         console.log("====> Aqui Fora")
+                     alert(`Usuário - cartas: ${soma} - Pontuação ${pontos - cartaUsuario.valor}\nComputador - cartas: ${somaComputador} - Pontuação ${pontosComputador - cartaComputador.valor}\nUsuário Ganhou!`);
+
+                  }else if(pontosComputador - cartaComputador.valor> pontos - cartaUsuario.valor){
+
+                     alert(`Usuário - cartas: ${soma} - Pontuação ${pontos - cartaUsuario.valor}\nComputador - cartas: ${somaComputador} - Pontuação ${pontosComputador - cartaComputador.valor}\nComputador Ganhou!`);
+
+                  }else{
+
+                     alert(`Usuário - cartas: ${soma} - Pontuação ${pontos - cartaUsuario.valor}\nComputador - cartas: ${somaComputador} - Pontuação ${pontosComputador - cartaComputador.valor}\nEmpate!`);
+                  }
+
+                  pontosComputador= 22;
+               }
+            }
+         }
       }
-
-
-      //Ganhador
-      // if(pontuacaoDoUsuario > pontuacaoDoComputador){
-      //    console.log("O usuário ganhou!");
-
-      // }else if(pontuacaoDoComputador > pontuacaoDoUsuario){
-      //    console.log("O computador ganhou!");
-
-      // }else{
-      //    console.log("Empate!");
-      // }
-      
    } else {
-      console.log("O jogo acabou!");
+      alert("O jogo acabou!");
    }
 };
 
