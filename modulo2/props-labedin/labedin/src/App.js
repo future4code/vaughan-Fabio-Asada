1d8cf34010c95824e3ed9b735629114a181c6d61
Fábio Asada <fabioasada@hotmail.com>
2022-01-11 20:07:15 -0300

    Arrumando alguns erros

--- modulo2/props-labedin/labedin/src/App.js
+++ modulo2/props-labedin/labedin/src/App.js
@@ -77,7 +77,7 @@ function App() {
 
         <CardGrande 
           imagem={js} 
-          nome="JediJavaScript"
+          nome="JavaScript"
           medida="100px" 
           descricao="JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma. Juntamente com HTML e CSS, o JavaScript é uma das três principais tecnologias da World Wide Web."/>
 
