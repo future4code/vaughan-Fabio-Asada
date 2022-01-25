e268278ca95cc99c643a5f8a18f90a2bd95ab39c
Fábio Asada <fabioasada@hotmail.com>
2022-01-25 20:29:10 -0300

    Tentado resolver os desafios

--- modulo2/apis-labenusers/apis-labenusers/src/App.js
+++ modulo2/apis-labenusers/apis-labenusers/src/App.js
@@ -10,6 +10,12 @@ class App extends React.Component{
     mudarTela:false,
     telaUsuario:false,
     nomeEmail:[],
+    edicao:false,
+    inputNomeEditar:"",
+    inputEmailEditar:"",
+    procurarUsuario:"",
+    usuarioBuscadoPorNome:"",
+    mostrarNomeBuscado:false,
   }
 
   onChangeNome= e => {
@@ -35,6 +41,8 @@ class App extends React.Component{
     axios
       .post(url, body, header)
       .then(response => alert("Usuário Criado com sucesso"))
+      this.setState({inputValueNome:""})
+      this.setState({inputValueEmail:""})
       .catch(error =>{ 
         console.log(error.response.data)
       alert("Ocorreu um erro ao criar Usuário.")})
@@ -71,6 +79,7 @@ class App extends React.Component{
           this.pegarUsuario()
           this.setState({inputValueNome:""})
           this.setState({inputValueEmail:""})
+          this.setState({nomeEmail:[]})
           alert("Usuário apagado com sucesso.")
         })
         .catch(error => {
@@ -100,19 +109,112 @@ class App extends React.Component{
         .get(url, header)
         .then(response => {
           
-          this.setState({nomeEmail:[ {nome:response.data.name}, {email:response.data.email}, {id:response.data.id}]})
+          this.setState({nomeEmail:[ {
+            nome:response.data.name,
+            email:response.data.email,
+            id:response.data.id
+          }]})
           
         })
         .catch(error => {
           console.log(error.response.data)
         })
+  }
+
+
+  infoDoUsuarioEditado= id => {
+
+    const url=`https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id}`;
+
+    const header= {headers:{
+      Authorization: "fabio-asada-vaughan"
+    }};
+
+    axios
+        .get(url, header)
+        .then(response => {
+          
+          this.setState({nomeEmail:[ {
+            nome:response.data.name,
+            email:response.data.email,
+            id:response.data.id
+          }]})
+          
+        })
+        .catch(error => {
+          console.log(error.response.data)
+        })
+  }
+
+  onClickEdicao= () => {
+    this.setState({edicao:!this.state.edicao})
+  }
+
+  editarNome= e => {
+    this.setState({inputNomeEditar: e.target.value})
+  }
+
+  editarEmail= e => {
+    this.setState({inputEmailEditar: e.target.value})
+  }
+
+
+  editarUsuario= id => {
+    const url= `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id}`;
+
+    const header= {headers:{
+      Authorization: "fabio-asada-vaughan"
+    }};
+
+    const body= {
+      "name": `${this.state.inputNomeEditar}`,
+      "email": `${this.state.inputEmailEditar}`,
+    }
     
+    axios
+      .put(url, body ,header)
+      .then(response => {
+        alert("Edição feita com sucesso!")
+      })
+      .catch(error => {
+        alert("Não foi possível editar.")
+      })
+
+      this.setState({inputNomeEditar:""})
+      this.setState({inputEmailEditar:""})
+      this.setState({edicao:!this.state.edicao})
+
+      this.infoDoUsuarioEditado(id);
+  }
+
+  componentDidUpdate(){
+    this.pegarUsuario();
+  }
+
+  onChangeProcurarUsuario= e => {
+    this.setState({procurarUsuario:e.target.value});
+
+  }
+
+  varrerLista= () => {
     
+    const url=`https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/search?name=${this.state.procurarUsuario}`;
+
+    const header= {headers:{
+      Authorization: "fabio-asada-vaughan"
+    }};
     
+    axios
+      .get(url, header)
+      .then(response => {
+        this.setState({usuarioBuscadoPorNome:response.data[0].name})
+
+        this.setState({mostrarNomeBuscado:!this.state.mostrarNomeBuscado})
+      })
+      .catch(error => alert("Usuário não encontrado"));
   }
 
   render(){
-    console.log(this.state)
     let nome;
     
       if(!this.state.mudarTela){
@@ -128,15 +230,44 @@ class App extends React.Component{
       </div>)
         
       }else{
-        nome=this.state.mostrarNomes.map(obj => {
+        nome=(<div>
+        {!this.state.mostrarNomeBuscado? this.state.mostrarNomes.map(obj => {
           return(
             <div className='nomesNaTela'>
               <p onClick={() => this.infoDoUsuario(obj.id)}>{obj.name}</p>
               <button className='botaoExcluir'
-               onClick={() => this.excluir(obj.id)}>excluir</button>
+              onClick={() => this.excluir(obj.id)}>excluir</button>
             </div>
           )
-        })
+        }):<div>
+          
+          {this.state.mostrarNomes.map(obj => {
+
+            if(obj.name === this.state.usuarioBuscadoPorNome){
+
+              return(
+                  <div className='nomesNaTela'>
+                    <p onClick={() => this. infoDoUsuario(obj.id)}>{obj.name}</p>
+                    <button   className='botaoExcluir'
+                    onClick={() => this.excluir (obj.id)}>excluir</button>
+                  </div>
+                    )
+            }else{
+              return '';
+            }
+                })}
+
+            </div>
+          }
+
+          <div>
+            <h3>Procurar Usuário</h3>
+            <input value={this.state.procurarUsuario} placeholder='Nome exato para busca' onChange={this.onChangeProcurarUsuario}/>
+
+            <button onClick={this.varrerLista}>Salvar edição</button>
+          </div>
+
+        </div>)
       }
       
       if(this.state.telaUsuario){
@@ -144,11 +275,22 @@ class App extends React.Component{
           {this.state.nomeEmail.map(obj => {
           return(<div>
             <p>{obj.nome}</p>
+
             <p>{obj.email}</p>
+
+            {this.state.edicao? <div>
+              <input value={this.state.inputNomeEditar} placeholder='Nome' onChange={this.editarNome}/>
+
+              <input value={this.state.inputEmailEditar}  placeholder='Email' onChange={this.editarEmail}/>
+
+              <button onClick={() => this.editarUsuario(obj.id)}>Salvar Edição</button>
+
+              </div>: <button onClick={this.onClickEdicao}>Edição</button>}
+
+            <button onClick={() => this.excluir(obj.id)}>Excluir</button>
+            <button onClick={() => this.setState({telaUsuario:!this.state.telaUsuario})}>Voltar</button>
           </div>)
         })}
-        <button onClick={() => this.excluir()}>Excluir</button>
-        
         </div> 
       }
 
